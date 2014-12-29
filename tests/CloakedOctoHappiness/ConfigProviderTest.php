099515dc877e6d856db9494465542677e757d47e
Benedict <benedict@phpdevel.net>
2014-12-29 21:30:22 +0100

    Test defaults

--- tests/CloakedOctoHappiness/ConfigProviderTest.php
+++ tests/CloakedOctoHappiness/ConfigProviderTest.php
@@ -5,6 +5,19 @@ use CloakedOctoHappiness\ConfigProvider;
 
 class ConfigProviderTest extends PHPUnit_Framework_TestCase
 {
+    public function testHasDefaults()
+    {
+        $c = new Container();
+        $c->register(new ConfigProvider());
+        $this->assertArrayHasKey('config.source', $c);
+        $this->assertArrayHasKey('config.destination', $c);
+        $this->assertArrayHasKey('config.include', $c);
+        $this->assertArrayHasKey('config.exclude', $c);
+        $this->assertArrayHasKey('config.keep_files', $c);
+        $this->assertArrayHasKey('config.timezone', $c);
+        $this->assertArrayHasKey('config.encoding', $c);
+    }
+
     public function testDefaultSourceDirIsCwd()
     {
         $c = new Container();
@@ -12,7 +25,7 @@ class ConfigProviderTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(getcwd(), $c['config.source']);
     }
 
-    public function testDefaultBuildDirIsWeb()
+    public function testDefaultDestinationDirIsWeb()
     {
         $c = new Container();
         $c->register(new ConfigProvider());
